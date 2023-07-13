82e9a4c7d8e1f413ec7107b027bf0dc65cbc9543
Duy Đan <89164062+DuyDan2706@users.noreply.github.com>
2023-07-13 16:34:04 +0700

    fix layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 export default function Header() {
   return (
     <div className='bg-blue-200 pb-5 pt-2 text-white'>
       <div className='mx-auto max-w-7xl py-4'>
         <div className='flex justify-end'>
-          <div className='cursor-poiter flex items-center py-1 hover:text-gray-300'>
-            <div className='flex-shink-0 ml-6 mr-2 h-6 w-6'>
+          <div className='cursor-pointer flex items-center py-1 hover:text-gray-300'>
+            <div className='flex-shrink-0 ml-6 mr-2 h-6 w-6'>
               <img
                 src='https://firebasestorage.googleapis.com/v0/b/subscriptionmilk.appspot.com/o/img%2FTD%20(2).png?alt=media&token=14f58401-fd62-4b6a-bf73-b6dcde64d9ad&_gl=1*191ikvn*_ga*NDI1NTcxNTI2LjE2Njg0MzU3MDI.*_ga_CW55HF8NVT*MTY4NTkzMDYzOC40MC4xLjE2ODU5MzA2NjEuMC4wLjA'
-                alt='avartar'
+                alt='avatar'
                 className='h-full w-full rounded-full object-cover'
               />
             </div>
@@ -19,7 +19,7 @@ export default function Header() {
         <div className='mt-4 grid grid-cols-12 items-end gap-4'>
           <div className='col-span-2'>
             <img
-              className='h-14 lg:h-14'
+              className='h-14 lg:h-14 object-contain'
               src='https://cdn0.fahasa.com/skin/frontend/ma_vanese/fahasa/images/logo/fahasa_logo.png'
               alt='dan111'
             />
@@ -32,7 +32,7 @@ export default function Header() {
                 className='flex-grow border-none bg-transparent px-3 py-2 text-black outline-none'
                 placeholder='Tìm kiếm sản phẩm mong muốn...'
               ></input>
-              <button className='flex-shirk-0 bg-orange-500 px-6 py-2  hover:opacity-70'>
+              <button className='flex-shrink-0 bg-orange-500 px-6 py-2 hover:opacity-70'>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   fill='none'
@@ -49,24 +49,6 @@ export default function Header() {
               </button>
             </div>
           </form>
-          <div className='col-span-1 ml-6 '>
-            <Link to='/' className=''>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='h-10 w-10'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
-                />
-              </svg>
-            </Link>
-          </div>
         </div>
       </div>
     </div>
--- src/page/Comment/CommentForm.jsx
+++ src/page/Comment/CommentForm.jsx
@@ -20,6 +20,7 @@ function Comment({ comment }) {
 
     setReplies([...replies, newReply])
     setReplyContent('')
+    setShowReplyForm(false)
   }
 
   const toggleReplyForm = () => {
@@ -27,11 +28,8 @@ function Comment({ comment }) {
   }
 
   return (
-    <div className='max-w-2xl mx-auto px-4'>
-      <div className='flex items-center'>
-        {/* <strong>{comment.user}</strong> */}
-        {/* <div className=' mx-4 text-gray-600 dark:text-gray-400'>{comment.date}</div> */}
-      </div>
+    <div className='max-w-4xl mx-auto px-4'>
+      <div className='flex items-center'></div>
       <article className='p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900'>
         <footer className='flex justify-between items-center mb-2'>
           <div className='flex items-center'>
@@ -78,7 +76,7 @@ function Comment({ comment }) {
           <button
             onClick={toggleReplyForm}
             type='button'
-            className='flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400'
+            className='flex items-center text-sm text-blue-500 hover:underline dark:text-white'
           >
             {showReplyForm ? (
               'Cancel Reply'
@@ -102,42 +100,17 @@ function Comment({ comment }) {
           </button>
         </div>
       </article>
-
-      {/* <button className='text-blue-500 hover:underline ' onClick={toggleReplyForm}>
-        {showReplyForm ? (
-          'Cancel Reply'
-        ) : (
-          <svg
-            aria-hidden='true'
-            className='mr-1 w-4 h-4'
-            fill='none'
-            stroke='currentColor'
-            viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
-            />
-          </svg>
-        )}
-      </button> */}
-
       {showReplyForm && (
         <form className='reply-form mt-4' onSubmit={handleReplySubmit}>
-          <div className='flex items-center mb-2'></div>
-          <div className='flex items-center mb-2'>
-            <label htmlFor='reply' className='mr-2'>
-              Add a reply:
-            </label>
+          <div className='flex items-center  py-2 px-4 mb-4'>
             <textarea
               id='reply'
               placeholder='Add a reply'
+              rows={6}
+              required
               value={replyContent}
               onChange={(e) => setReplyContent(e.target.value)}
-              className='p-2 border border-gray-300 rounded'
+              className='px-0 w-full border border-gray-300 rounded'
             />
           </div>
           <button type='submit' className='py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600'>
